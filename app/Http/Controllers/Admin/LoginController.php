86c339d6413e758763cf3ad3368c691b7eda4ef7
shuvo2k <findariful@gmail.com>
2019-08-18 13:00:29 +0600

    admin login added

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,8 +4,45 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect admins after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+
+
+
+/**
+ * @param Request $request
+ * @return \Illuminate\Http\RedirectResponse
+ * @throws \Illuminate\Validation\ValidationException
+**/
+public function login(Request $request){
+
+}
 }
